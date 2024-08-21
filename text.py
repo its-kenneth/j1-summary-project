dbdf735c85b0b04ce5ebc13f5c1aeaefd73be71a
e-say1 <120434420+e-say1@users.noreply.github.com>
2024-08-21 01:45:43 +0000

    rebalanced moves

--- text.py
+++ text.py
@@ -15,17 +15,15 @@ moves = [
     {"name": "Slash", "multiplier": 2.0, "power_limit": 5},
     {"name": "Shot", "multiplier": 2.5, "power_limit": 3},
     {"name": "Harass", "multiplier": 3.5, "power_limit": 3},
-    {"name": "Squash", "multiplier": 1.5, "power_limit": 10},
     {"name": "Cry", "multiplier": 2, "power_limit": 6}
 ]
 
 creatures = [
     {"name": "Toss Boss", "hp": 200, "attack": 10, "move_dropped": "Throw"},
     {"name": "Silver Knight", "hp": 450, "attack": 25, "move_dropped": "Slash"},
+    {"name": "Little Boy", "hp": 300, "attack": 55, "move_dropped": "Cry"},
     {"name": "Captain Gun", "hp": 800, "attack": 45, "move_dropped": "Shot"},
     {"name": "Manuel", "hp": 600, "attack": 60, "move_dropped": "Harass"},
-    {"name": "Big Guy", "hp": 800, "attack": 10, "move_dropped": "Squash"},
-    {"name": "Little Boy", "hp": 300, "attack": 55, "move_dropped": "Cry"}
 ]
 
 monster = {"name": "Monster", "hp": 1000, "attack": 65}
